d0d2ec901226dd1b507a13a770f66ec1174c8204
qannoufoualid <oualidqannouf@gmail.com>
2021-01-31 15:57:43 +0100

    FEAT | création de markers, popups & cercles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { latLng, tileLayer } from 'leaflet';
+import { latLng, marker, tileLayer, circle, Map } from 'leaflet';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +7,7 @@ import { latLng, tileLayer } from 'leaflet';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
 
   options = {
     layers: [
@@ -16,4 +17,19 @@ export class AppComponent {
     center: latLng(45.7640, 4.8357)
   };
 
+  onMapReady(map: Map): void {
+    // ajouter un marker
+    const marker1 = marker([45.7640, 4.8357]).addTo(map);
+
+    // associer une popup au click sur le marker
+    marker1.bindPopup('<b>Salut!</b><br>Je suis une popup').openPopup();
+
+    // Ajouter un cercle
+    circle([45.7640, 4.9], {
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.5,
+      radius: 500
+    }).addTo(map);
+  }
 }
